429bc365521940ecbc3cb5559f62f6288e9a13e2
David Cox <david@integrated-reasoning.com>
2023-12-15 08:18:16 -1000

    feat(lib.rs, file.rs): add MPSFile

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: usize, right: usize) -> usize {
-  left + right
-}
+pub mod file;
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
+  //#[test]
+  //fn todo() { }
 }
