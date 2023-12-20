21c634bafa09c6ea780ceb31feac337b3250a32a
David Cox <david@integrated-reasoning.com>
2023-12-20 14:53:02 -0600

    test(benchmark.rs): add benchmark.rs

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,9 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use mps::parser;
+use mps::file::MPSFile;
 
 fn criterion_benchmark(c: &mut Criterion) {
   let a = "NAME          AFIRO\n";
-  c.bench_function("name", |b| b.iter(|| parser::name(black_box(a))));
+  c.bench_function("name", |b| b.iter(|| MPSFile::<f32>::name(black_box(a))));
 }
 
 criterion_group!(benches, criterion_benchmark);
