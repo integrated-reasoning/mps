145e4e2fd35539921292f5c3110276da54bd381e
David Cox <david@integrated-reasoning.com>
2023-12-22 15:43:45 -0600

    feat(main.rs, cli.rs): add cli

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod cli;
+use color_eyre::{eyre::eyre, Result};
+mod file;
+use clap::Parser;
+use cli::Cli;
+use file::MPSFile;
+use std::fs;
+
+fn main() -> Result<()> {
+  let args = Cli::parse();
+  let contents = fs::read_to_string(&args.input_path)?;
+  match MPSFile::<f32>::parse(&contents) {
+    Ok((_, parsed)) => Ok(println!("{:#?}", parsed)),
+    Err(e) => Err(eyre!(e.to_string())),
+  }?;
+  Ok(())
+}
