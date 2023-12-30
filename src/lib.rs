2e8aeea93fe22977cdde5d782d00553cef9f76af
David Cox <david@integrated-reasoning.com>
2023-12-30 14:44:05 -0800

    docs(lib.rs): update usage example

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,48 @@
 //!
 //! **Library**
 //!
-//! ```ignore
+//! ```
 //! use mps::Parser;
 //!
-//! let contents = "MPS data...";
-//! match Parser::<f32>::parse(&contents) {
-//!     Ok((_, model)) => { /* use MPS model */ },
-//!     Err(e) => eprintln!("Parsing error: {}", e),
+//! let data = "
+//! NAME example
+//! ROWS
+//!  N  OBJ
+//!  L  R1
+//!  L  R2
+//!  E  R3
+//! COLUMNS
+//!     X1        OBJ       -6
+//!     X1        R1        2
+//!     X1        R2        1
+//!     X1        R3        3
+//!     X2        OBJ       7
+//!     X2        R1        5
+//!     X2        R2        -1
+//!     X2        R3        2
+//!     X3        OBJ       4
+//!     X3        R1        -1
+//!     X3        R2        -2
+//!     X3        R3        2
+//! RHS
+//!     RHS1      R1        18
+//!     RHS1      R2        -14
+//!     RHS1      R3        26
+//! BOUNDS
+//!  LO BND1      X1        0
+//!  LO BND1      X2        0
+//!  LO BND1      X3        0
+//! ENDATA";
+//!
+//! cfg_if::cfg_if! {
+//!   if #[cfg(feature = "located")] {
+//!     use nom_locate::LocatedSpan;
+//!     use nom_tracable::TracableInfo;
+//!     let info = TracableInfo::new().forward(true).backward(true);
+//!     Parser::<f32>::parse(LocatedSpan::new_extra(data, info));
+//!   } else {
+//!     Parser::<f32>::parse(data);
+//!   }
 //! }
 //! ```
 //!
