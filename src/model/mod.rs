7515e74bebba9f975f43fe6d401ba6c9164faad8
David Cox <david@integrated-reasoning.com>
2024-01-05 15:28:22 -1000

    feat(Model): implement RowColumnValueMap::try_from

---
+++ src/model/mod.rs
@@ -0,0 +1,56 @@
+mod row_column_value_map;
+mod row_type_map;
+
+use crate::model::row_column_value_map::RowColumnValueMap;
+use crate::model::row_type_map::RowTypeMap;
+use crate::types::Parser;
+use color_eyre::Result;
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct Model {
+  pub name: String,
+  pub row_types: RowTypeMap,
+  pub values: RowColumnValueMap,
+}
+
+impl TryFrom<Parser<'_, f32>> for Model {
+  type Error = color_eyre::Report;
+
+  fn try_from(parsed: Parser<f32>) -> Result<Self> {
+    let row_types = RowTypeMap::try_from(&parsed.rows)?;
+    let values = RowColumnValueMap::try_from((&parsed.columns, &row_types))?;
+    Ok(Model {
+      name: parsed.name.to_string(),
+      row_types,
+      values,
+    })
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use color_eyre::{eyre::eyre, Result};
+
+  #[test]
+  fn test_try_from_afiro() -> Result<()> {
+    let (_, parsed) =
+      Parser::<f32>::parse(include_str!("../../tests/data/netlib/afiro"))?;
+    Model::try_from(parsed)?;
+    Ok(())
+  }
+
+  #[test]
+  fn test_conflicting_rows_line() -> Result<()> {
+    let (_, parsed) = Parser::<f32>::parse(include_str!(
+      "../../tests/data/should_fail/conflicting_rows_line"
+    ))?;
+    let error =
+      eyre!("conflicting row type information for R09: found Leq and Eq");
+    match Model::try_from(parsed) {
+      Ok(_) => panic!(),
+      Err(e) => assert_eq!(e.to_string(), error.to_string()),
+    };
+    Ok(())
+  }
+}
---
+++ src/model/row_type_map.rs
@@ -0,0 +1,51 @@
+use crate::types::{Parser, RowType, Rows};
+use color_eyre::{eyre::eyre, Result};
+use hashbrown::HashMap;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct RowTypeMap(HashMap<String, RowType>);
+
+impl TryFrom<&Rows<'_>> for RowTypeMap {
+  type Error = color_eyre::Report;
+
+  fn try_from(rows: &Rows<'_>) -> Result<Self> {
+    let mut row_types = HashMap::new();
+    for r in rows {
+      match row_types.insert(r.row_name.to_string(), r.row_type.clone()) {
+        Some(row_type) => Err(eyre!(format!(
+          "conflicting row type information for {}: found {:?} and {:?}",
+          r.row_name,
+          r.row_type.clone(),
+          row_type
+        ))),
+        None => Ok(()),
+      }?;
+    }
+    Ok(RowTypeMap(row_types))
+  }
+}
+
+impl RowTypeMap {
+  pub fn get(&self, row_name: &str) -> Option<&RowType> {
+    self.0.get(row_name)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_conflicting_rows_line() -> Result<()> {
+    let (_, parsed) = Parser::<f32>::parse(include_str!(
+      "../../tests/data/should_fail/conflicting_rows_line"
+    ))?;
+    let error =
+      eyre!("conflicting row type information for R09: found Leq and Eq");
+    match RowTypeMap::try_from(&parsed.rows) {
+      Ok(_) => panic!(),
+      Err(e) => assert_eq!(e.to_string(), error.to_string()),
+    };
+    Ok(())
+  }
+}
